68737b4b1d82c37bc8db422407ebb97e328dd581
arcismd <a.sincarenco.97@gmail.com>
2022-08-09 18:12:21 +0300

    feat: add validation for login register and posts creating

---
+++ server/validations/validation.js
@@ -0,0 +1,20 @@
+import { body } from 'express-validator';
+
+export const loginValidation = [
+    body('password', 'Parola nu corespunde ori este prea scurtă').isLength({ min: 6 }), 
+    body('userName', 'Numele este incorect').isLength({ min: 3 }),
+];
+
+export const registerValidation = [
+    body('email', 'Poșta are un format incorect').isEmail(),
+    body('password', 'Parola este prea scurtă').isLength({ min: 6 }), 
+    body('userName', 'Introdu un nume compus din minim 3 caractere').isLength({ min: 3 }),
+    body('avatarUrl', 'Adresă greșită pentru poza de profil').optional().isURL(),
+];
+
+export const postCreateValidation = [
+    body('title', 'Introduceţi denumirea titlului').isLength({ min: 3 }).isString(),
+    body('text', 'Introduceţi textul pentru articol').isLength({ min: 10 }).isString(), 
+    body('tags', 'Tag-urile introduse greșit').optional().isString(),
+    body('imageUrl', 'Adresă greșită pentru imagine').optional().isString(),
+];
