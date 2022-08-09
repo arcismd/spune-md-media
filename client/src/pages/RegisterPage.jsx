7ae9bba904edbcdec07de6040ece2363e75e7277
arcismd <a.sincarenco.97@gmail.com>
2022-08-10 01:41:29 +0300

    feat: implement registration on client and server parts

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchRegister, selectIsAuth } from '../redux/slices/auth';
 
 export const RegisterPage = () => {
+
+  const isAuth = useSelector(selectIsAuth);
+  const dispatch = useDispatch();
+  const [errLog, seterrLog] = useState('');
+
+  const { 
+    register, 
+    handleSubmit, 
+ } = useForm({
+    defaultValues: {
+      userName: '',
+      email: '',
+      password: ''
+    },
+    mode: 'onChange',
+  });
+
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchRegister(values));
+
+    if (!data.payload) {
+      return seterrLog('Introduceți coerent datele')
+    }
+
+    if ('token' in data.payload) {
+      window.localStorage.setItem('token', data.payload.token);
+    } 
+  };
+
+  if (isAuth) {
+    return <Navigate to='/'/>
+  }
+
+
   return (
     <div>
     <div className='login-register-switcher'>
@@ -20,12 +58,13 @@ export const RegisterPage = () => {
         <div className='register-left-side-bar'>
         </div>
         <div className='register-right-side-bar'>
-        <form onSubmit={e => e.preventDefault()}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <label>
               Nume de utilizator*
               <input 
                 type='text'
-                placeholder='utilizator'
+                placeholder='min. 3 caractere'
+                { ...register('userName', { required: 'Scrie numele' })}
                 className=''
               />
             </label>
@@ -35,6 +74,7 @@ export const RegisterPage = () => {
               <input 
                 type='email'
                 placeholder='pepe@gmail.com'
+                { ...register('email', { required: 'Scrie emailul' })}
                 className=''
               />
             </label>
@@ -44,20 +84,13 @@ export const RegisterPage = () => {
               <input 
                 type='password'
                 placeholder='min. 6 caractere'
-                className=''
-              />
-            </label>
-
-            <label>
-              Repetă Parola*
-              <input 
-                type='password'
-                placeholder='parolă'
+                { ...register('password', { required: 'Scrie parola' })}
                 className=''
               />
             </label>
 
             <button type='submit' className='reg-btn'>Creează un cont</button>
+            <p style={{color: '#B15557'}}>{errLog}</p>
         </form>
         </div>
       </div>
