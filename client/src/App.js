52cca86fb0b8ef88f77ebd392b6cb6fd54488380
arcismd <a.sincarenco.97@gmail.com>
2022-08-09 17:43:53 +0300

    feat: configure the routes using router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
+import { Layout } from "./components/Layout";
+import { Routes, Route } from 'react-router-dom';
+import { MainPage } from './pages/MainPage';
+import { PostPage } from './pages/PostPage';
+import { AddPostPage } from './pages/AddPostPage';
+import { EditPostPage } from './pages/EditPostPage';
+import { LoginPage } from './pages/LoginPage';
+import { RegisterPage } from './pages/RegisterPage';
+
 function App() {
   return (
-    <div className="App"></div>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path=':id' element={<PostPage />} />
+        <Route path=':id/edit' element={<EditPostPage />} />
+        <Route path='new' element={<AddPostPage />} />
+        <Route path='login' element={<LoginPage />} />
+        <Route path='register' element={<RegisterPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
 export default App;
+
