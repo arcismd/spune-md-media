e9ae7e7f38356447a84750301c453cb76773fdda
arcismd <a.sincarenco.97@gmail.com>
2022-08-09 18:04:01 +0300

    feat: connect to database

--- server/index.js
+++ server/index.js
@@ -1,3 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import {
+    registerValidation,
+    loginValidation,
+    postCreateValidation
+} from './validations/validation.js';
+import checkAuth from './utils/checkAuth.js';
+import * as UserControllers from './controllers/UserController.js';
+import * as PostControllers from './controllers/PostController.js';
+
+mongoose.connect('mongodb+srv://admin:123456!@spune.apuiol2.mongodb.net/spune?retryWrites=true&w=majority', )
+    .then(() => console.log('DB OK'))
+    .catch((err) => console.log('DB ERROR', err));
 
 const app = express();
