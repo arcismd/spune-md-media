ebcc0cd8c027789d5a53ef10a251343160b73c6a
arcismd <a.sincarenco.97@gmail.com>
2022-08-10 14:49:02 +0300

    feat: convert timestamps to time ago format

--- client/src/components/UserInfo/UserInfo.jsx
+++ client/src/components/UserInfo/UserInfo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import ReactTimeAgo from 'react-time-ago'
 
 export const UserInfo = ({ avatarUrl, userName, additionalText }) => {
   return (
     <div className='user-info-module'>
       <img className='user-post-avatar' src={avatarUrl || 'https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg'} alt={userName} />
       <span className='username'>{userName}</span>
-        <span className='created-date'>{additionalText}</span>
+        <span className='created-date'><ReactTimeAgo date={additionalText} /></span>
     </div>
   );
 };
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,10 @@ import store from './redux/store';
 import './styles/styles/styles.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import TimeAgo from 'javascript-time-ago'
+import ro from 'javascript-time-ago/locale/ro.json'
+
+TimeAgo.addDefaultLocale(ro)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
